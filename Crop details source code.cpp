f36621cfbf748332f85151219938349c2b106747
NITROXITO <129105529+NITROXITO@users.noreply.github.com>
2023-03-27 23:43:30 +0530

    Add files via upload

---
+++ Crop details source code.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+struct Crop {
+    int crop_id;
+    string crop_name;
+    string crop_type;
+    string region;
+    string plantation_season;
+};
+
+int main() {
+    Crop crops[5];
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter Crop ID: ";
+        cin >> crops[i].crop_id;
+        cin.ignore();
+
+        cout << "Enter Crop Name: ";
+        getline(cin, crops[i].crop_name);
+
+        cout << "Enter Crop Type: ";
+        getline(cin, crops[i].crop_type);
+
+        cout << "Enter Region: ";
+        getline(cin, crops[i].region);
+
+        cout << "Enter Plantation Season: ";
+        getline(cin, crops[i].plantation_season);
+
+        cout << endl;
+    }
+
+    cout << "Crops Entered:\n\n";
+    for (int i = 0; i < 5; i++) {
+        cout << "Crop ID: " << crops[i].crop_id << endl;
+        cout << "Crop Name: " << crops[i].crop_name << endl;
+        cout << "Crop Type: " << crops[i].crop_type << endl;
+        cout << "Region: " << crops[i].region << endl;
+        cout << "Plantation Season: " << crops[i].plantation_season << endl << endl;
+    }
+
+    return 0;
+}
